69a40f8245bb543715b66bb372570eb531c776eb
Kevin Duck <kevingduck@gmail.com>
2020-11-30 22:22:32 -0500

    Add tesseract OCR function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Tesseract from 'tesseract.js';
+import ImageUploader from 'react-images-upload';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { pictures: [] };
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(picture) {
+        this.setState({
+            pictures: this.state.pictures.concat(picture),
+        });
+        console.log("Added picture to state. Recognizing text ...")
+        Tesseract.recognize(
+            'https://tesseract.projectnaptha.com/img/eng_bw.png',            
+            'eng',
+            { logger: m => console.log(m) }
+          ).then(({ data: { text } }) => {
+            console.log(text);
+          })
+    }
+
+    render() {
+        return (
+            <div>
+                <ImageUploader
+                    withIcon={true}
+                    withPreview={true}
+                    buttonText='Choose images'
+                    onChange={this.onChange}
+                    imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                    maxFileSize={5242880}
+                />
+            </div>
+        );
+    }
 }
 
-export default App;
+export default App
